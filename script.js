fc00248d8ef6b77158829d3d0e64016f22b33041
ZeroToThePower1 <168172667+ZeroToThePower1@users.noreply.github.com>
2025-09-07 18:26:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const API_BASE = 'http://localhost:3000/api';
+const API_BASE = 'https://attendance-server-nkxx.onrender.com/api';
 
 
 const dateFilter = document.getElementById('date-filter');
@@ -366,4 +366,5 @@ function showError(message) {
     
     console.error(message);
     alert(message); 
+
 }
